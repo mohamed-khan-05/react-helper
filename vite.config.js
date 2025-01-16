a9168c6e131443912393086db419fb2988590241
Mohamed <mohamedkhan050124@gmail.com>
2025-01-16 11:29:25 +0200

    viteconfig

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://mohamed-khan-05.github.io/react-helper/",
+  base: "/react-helper/",
 });
